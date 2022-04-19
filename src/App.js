43d361877f4501f41a53fe9ac81089129f95b502
Cam <cama0047@algonquinlive.com>
2022-04-18 23:06:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import qr from './Assets/image-qr-code.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <div className="card">
+        <div className="QRContainer">
+            <img alt="qr code img" src={qr}></img>
+        </div>
+        <div className = "QRBody">
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next lavel</p>
+        </div>
+      </div>
+
       </header>
     </div>
   );
---
+++ src/Components/Qrcode.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Qrcode(){
+
+return (
+    <div>
+        QrCode
+    </div>
+)
+
+}
+export default Qrcode;
